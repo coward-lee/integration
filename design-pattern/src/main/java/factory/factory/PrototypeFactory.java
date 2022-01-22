62a4dac8a0c27811ad28edd96bf3117dbb6dae6c
coward-lee <1398400350@qq.com>
2022-01-23 00:05:29 +0800

    。。。

---
+++ design-pattern/src/main/java/factory/factory/PrototypeFactory.java
@@ -0,0 +1,73 @@
+package factory.factory;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 原型类
+ * 就是有一个基本的类，但是每次的对类进行使用的使用可复制一个对象出去给调用者调用
+ */
+public class PrototypeFactory {
+    Map<String, Object> map = new HashMap<>();
+    Object getObj(String name) throws Exception {
+        Object instance = map.get(name);
+        if (instance == null){
+            instance = new PrototypeObj("原型",new RefClass("原型的应用类"));
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+        oos.writeObject(instance);
+
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(outputStream.toByteArray()));
+        return ois.readObject();
+    }
+
+    public static void main(String[] args) throws Exception {
+        PrototypeFactory prototypeFactory = new PrototypeFactory();
+        Object as = prototypeFactory.getObj("as");
+        System.out.println(as);
+        System.out.println( prototypeFactory.getObj("as"));
+        System.out.println( prototypeFactory.getObj("as"));
+        System.out.println( prototypeFactory.getObj("as"));
+    }
+
+    public static class PrototypeObj implements Serializable{
+        public PrototypeObj(String name, RefClass refClass) {
+            this.name = name;
+            this.refClass = refClass;
+        }
+
+        String name;
+        RefClass refClass;
+
+        @Override
+        public String toString() {
+            return "PrototypeObj{" +
+                    "name='" + name + '\'' +
+                    ", refClass=" + refClass +
+                    '}';
+        }
+
+
+    }
+    public static class RefClass implements Serializable{
+        public RefClass(String ref) {
+            this.ref = ref;
+        }
+
+        String ref;
+
+        @Override
+        public String toString() {
+            return "RefClass{" +
+                    "ref='" + ref + '\'' +
+                    '}';
+        }
+    }
+
+
+}
+
+
+
--- spring-boot/src/main/java/org/lee/Controller.java
+++ spring-boot/src/main/java/org/lee/Controller.java
@@ -2,6 +2,7 @@ package org.lee;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 
+@Service
 @RestController
 public class Controller {
     Logger log = LoggerFactory.getLogger(Controller.class);
