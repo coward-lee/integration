b84191cb8d387426ef9e549804d6e391b7f02657
liqiang <liqiang@betalpha.com>
2022-02-14 10:17:02 +0800

    string

--- basic/src/main/java/org/lee/study/refleact/ChangeStringDemo.java
+++ basic/src/main/java/org/lee/study/refleact/ChangeStringDemo.java
@@ -1,2 +1,7 @@
-package org.lee.study.refleact;public class ChangeStringDemo {
+package org.lee.study.refleact;
+
+public class ChangeStringDemo {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> aClass = Class.forName(String.class.getName());
+    }
 }
