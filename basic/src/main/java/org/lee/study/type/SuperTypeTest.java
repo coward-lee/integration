28c68c1824f66b95eb15aa486d215ae49fdb94dd
liqiang <liqiang@betalpha.com>
2022-03-21 13:43:04 +0800

    builder record

--- basic/src/main/java/org/lee/study/type/SuperTypeTest.java
+++ basic/src/main/java/org/lee/study/type/SuperTypeTest.java
@@ -12,6 +12,9 @@ public class SuperTypeTest{
         List<? super Serializable> a = Arrays.asList(1.2 , 1, 2);
         PrintStream printStream = new PrintStream("a");
         printStream.println(a);
+        // 编译错误
+//        原因: 无法将 '<>' 与匿名内部类一起使用
+//        Comparable<String> comparable = new Comparable<>() {@Override public int compareTo(String o) {return 0;}};
     }
 }
 
